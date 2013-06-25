db3c961540d728df7b8623a1917bfc9e9556daaa
mnursalam <m.nursalam@kiranatama.com>
2013-06-24 21:27:29 -0700

    Configurations

--- MvcTesting/Models/dbContext.cs
+++ MvcTesting/Models/dbContext.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.Configuration;
 
 namespace MvcTesting.Models
 {
-    public class ActDBContext : DbContext
+    public class ActDBContext : DbContext        
     {
-        public DbSet<Act> Acts { get; set; }
+        public static string ConnectionString { get; set; }
+        public ActDBContext() : base(ConnectionString ?? "dbconn1")
+        {
+        }
+        public DbSet<Act> Acts { get; set; } 
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Store> Stores { get; set; }        
     }
